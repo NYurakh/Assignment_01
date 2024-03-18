dbacdea9017793661d4d04b8b4e8fa894a5b43e2
s28403 <s28403@pjwstk.edu.pl>
2024-03-18 18:39:35 +0100

    Modification 2

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -2,3 +2,7 @@
 Console.WriteLine("What is your name?");
 string userName = Console.ReadLine() ?? "Anonymous";
 Console.WriteLine("Hello, " + userName);
+
+Console.WriteLine("How old are you?");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Your age is: " + age);
