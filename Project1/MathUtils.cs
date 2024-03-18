f88c79c10ca32adcdc5386de709ac56dec8f8101
s28403 <s28403@pjwstk.edu.pl>
2024-03-18 19:09:29 +0100

    Implemented FindMax method

--- Project1/MathUtils.cs
+++ Project1/MathUtils.cs
@@ -14,4 +14,23 @@ public class MathUtils
         }
         return (double)sum / numbers.Length;
     }
+
+
+    public static int FindMax(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be null or empty");
+            }
+    
+            int max = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            return max;
+        }
 }
